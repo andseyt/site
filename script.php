720b60130abd867142d680c883275a983579b65c
andseyt <140057214+andseyt@users.noreply.github.com>
2023-07-23 14:36:51 +0300

    script.php

--- script.php
+++ script.php
@@ -1,19 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Ваше сообщение успешно доставлено</title>
+</head>
+<body>
 <?php
-$db = mysqli_connect("localhost", "root", "");
 $name = $_POST["sirname"];
 $email = $_POST["email"];
 $text_message = $_POST["message"];
-
+$db=mysqli_connect("localhost", "root","", "site");
 $sql = "INSERT INTO test(name, email, message) VALUES ('$name', '$email', '$text_message')";
-$result = mysqli_query($db, $sql);
-
-if ($db == false){
-    print("Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error());
-}
-else {
-    print("Соединение установлено успешно");
-}
-if ($result == false) {
-    print("Произошла ошибка при выполнении запроса");
-}
+$result = mysqli_query($db, $sql) or die("Ошибка в запросе!".mysql_error());
 ?>
+</body>
+</html>
