f6cad3e60c367a557795384a77978d9473c99f1e
andseyt <140057214+andseyt@users.noreply.github.com>
2023-07-25 01:26:03 +0300

    Add files via upload

--- script.php
+++ script.php
@@ -1,17 +1,11 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Ваше сообщение успешно доставлено</title>
-</head>
-<body>
 <?php
+
 $name = $_POST["sirname"];
 $email = $_POST["email"];
 $text_message = $_POST["message"];
-$db=mysqli_connect("localhost", "root","", "site");
-$sql = "INSERT INTO test(name, email, message) VALUES ('$name', '$email', '$text_message')";
-$result = mysqli_query($db, $sql) or die("Ошибка в запросе!".mysql_error());
+
+$db=mysqli_connect("localhost", "root","", "test");
+
+$sql = "INSERT INTO test(name, email, message) VALUES ('$name', '$email','$text_message')";
+$result=mysqli_query($db, $sql) or die("Ошибка в запросе!".mysql_error());
 ?>
-</body>
-</html>
